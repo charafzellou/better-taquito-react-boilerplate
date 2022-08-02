241438c3430c71a529a89ce92f0f9a5bf2b34910
Charaf Zellou <charaf.zellou@smart-chain.fr>
2022-08-02 12:09:23 +0200

    Adding NodeJS CI Actions (#1)
    * Adding NodeJS CI Actions
    
    * Updated gitignore and GH Actions
    
    * Updated + Fixed Versions for all NPM Packages
    
    * Updated crypto-browserify
    
    * Updated AirGap NPM Packages
    
    * Working Release
    
    * Misc Updates

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -52,8 +52,8 @@ const ConnectButton = ({
     try {
       await wallet.requestPermissions({
         network: {
-          type: NetworkType.HANGZHOUNET,
-          rpcUrl: "https://hangzhounet.api.tez.ie"
+          type: NetworkType.GHOSTNET,
+          rpcUrl: "https://rpc.tzkt.io/ghostnet"
         }
       });
       // gets user's address
@@ -87,7 +87,7 @@ const ConnectButton = ({
       // creates a wallet instance
       const wallet = new BeaconWallet({
         name: "Taquito Boilerplate",
-        preferredNetwork: NetworkType.HANGZHOUNET,
+        preferredNetwork: NetworkType.GHOSTNET,
         disableDefaultEvents: true, // Disable all events / UI. This also disables the pairing alert.
         eventHandlers: {
           // To keep the pairing alert, we have to add the following default event handlers back
