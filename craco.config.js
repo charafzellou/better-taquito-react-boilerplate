a3641be3ef580e8ba8d56a748b7749d744f3193e
Charaf ZELLOU <charaf.zellou@gmail.com>
2026-03-12 11:18:18 +0100

    fix: added browser-compatible fallbacks for webpack configuration in craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -2,24 +2,33 @@ const webpack = require("webpack");
 
 module.exports = {
   webpack: {
-    configure: {
-      resolve: {
-        fallback: {
-          process: require.resolve("process/browser"),
-          zlib: require.resolve("browserify-zlib"),
-          stream: require.resolve("stream-browserify"),
-          util: require.resolve("util"),
-          buffer: require.resolve("buffer"),
-          asset: require.resolve("assert"),
-          crypto: require.resolve("crypto-browserify"),
-        },
-      },
-      plugins: [
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        process: require.resolve("process/browser.js"),
+        zlib: require.resolve("browserify-zlib"),
+        stream: require.resolve("stream-browserify"),
+        util: require.resolve("util"),
+        buffer: require.resolve("buffer"),
+        asset: require.resolve("assert"),
+        crypto: require.resolve("crypto-browserify"),
+      };
+
+      webpackConfig.plugins.push(
         new webpack.ProvidePlugin({
           Buffer: ["buffer", "Buffer"],
-          process: "process/browser",
-        }),
-      ],
+          process: "process/browser.js",
+        })
+      );
+
+      webpackConfig.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      });
+
+      return webpackConfig;
     },
   },
 };
